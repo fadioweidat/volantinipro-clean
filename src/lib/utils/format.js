7125c82c1d0c3b66eb4855578dc60f65510070cb
Fadi Oweidat <fenice.sp@gmail.com>
2026-06-03 10:44:37 +0200

    Restore format utils

---
+++ src/lib/utils/format.js
@@ -0,0 +1,15 @@
+﻿export function formatNumber(value) {
+  const n = Number(value || 0);
+  return new Intl.NumberFormat("it-IT").format(n);
+}
+
+export const formatNumero = formatNumber;
+
+export function formatCurrency(value) {
+  const n = Number(value || 0);
+  return new Intl.NumberFormat("it-IT", {
+    style: "currency",
+    currency: "EUR",
+    maximumFractionDigits: 0,
+  }).format(n);
+}
